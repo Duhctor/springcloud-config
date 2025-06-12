d504e5eac18e50a1d6634b24562a73ffbecad405
aven <aven@dynadot.com>
2025-06-12 09:11:21 +0800

    合并提交

---
+++ GitTest.java
@@ -0,0 +1,7 @@
+public class GitTest {
+  public static void main(String[] args) {
+    System.out.println("this is first git");
+    System.out.println("this is second git");
+    System.out.println("this is forth git");
+  }
+}
